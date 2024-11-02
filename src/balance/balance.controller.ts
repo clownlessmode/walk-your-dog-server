eaad75282101629185cd22088d7a4e03f6ac217f
clowlnessmode <clownessmode@bk.ru>
2024-11-02 19:55:44 +0700

    balance admin

--- src/balance/balance.controller.ts
+++ src/balance/balance.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { BalanceService } from './balance.service';
+import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
+class BalanceDto {
+  @ApiProperty({ example: 1900 })
+  @IsNumber()
+  amount: number;
+}
+
+
+@ApiTags('Balance')
 @Controller('balance')
 export class BalanceController {
   constructor(private readonly balanceService: BalanceService) {}
+
+
+
+
+  @ApiOperation({ summary: 'Set balance' })
+  @Post(':id')
+  async set(@Param('id') id: string, @Body() amount: BalanceDto) {
+    return await this.balanceService.addBalance(id, amount.amount);
+  }
 }
--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+import { Balance } from './entities/balance.entity';
 
 @Injectable()
-export class BalanceService {}
+export class BalanceService {
+  constructor(private readonly manager: EntityManager) {}
+
+  async addBalance(id: string, amount: number): Promise<Balance> {
+    const balance = await this.manager.findOne(Balance, { where: { id: id } });
+    if (!balance) throw new NotFoundException('Balance not found');
+
+    balance.general += amount;
+    return this.manager.save(balance);
+  }
+}
